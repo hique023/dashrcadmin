69d8c707fbda253184e4b4cec972b57899d183a5
hique023 <pedroviski10@hotmail.com>
2022-02-03 13:49:50 -0300

    Criado estrutura inicial da tela Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,41 @@
 // Global
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 // Styles
 import "./styles.css";
 
-export default function Login() {
+export default function Logon() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="loginContainer">
-      <h1>Login</h1>
+    <div className="container-master">
+      <div className="logon-container">
+        <section className="form">
+          {/* <form onSubmit={handleLogin}> */}
+          <form>
+            {/* <h1>Faça seu login</h1> */}
+
+            <input
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+              placeholder="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button className="button" type="submit">
+              Entrar
+            </button>
+          </form>
+        </section>
+      </div>
     </div>
   );
 }
